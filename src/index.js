c946614eeb014a705cce13f3adabe58bfeb24311
shinkare90 <55100900+Shinkarev90@users.noreply.github.com>
2021-03-04 20:49:05 +0400

    feat: update index file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+      return 0
+  } else {
+  array.sort((a,b) => a-b);
+  return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+      return 0
+  } else {
+  array.sort((a,b) => b-a);
+  return array[0];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+      return 0
+  } else {
+  let sum = 0;
+  for (let index = 0; index < array.length; index++) {
+      sum += array[index];
+  }
+  sum /= array.length;
+  return sum;
+  }
 }
